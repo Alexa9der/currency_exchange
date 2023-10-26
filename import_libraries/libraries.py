c55bde169a881098d53b814f2f9d633a25cb64c2
Alex <sashakaloshh@gmail.com>
2023-10-26 20:35:21 +0200

    added classes preprocessing

---
+++ import_libraries/libraries.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+import yfinance as yf
+from datetime import datetime
+import talib as tl
+# import MetaTrader5 as mt5 # treiding platforms
+
+import matplotlib.pyplot as plt 
+import seaborn           as sns
+
+from datetime import datetime, timedelta
+import time
+
+# интерактивные виджеты
+# @interact
+import ipywidgets as widgets
+from ipywidgets import interact, interact_manual
+
+# Jupyter Notebook progressbar decorator
+# from tqdm import tqdm_notebook as tqdm
+
+import inspect
